03732fe036570a1c4841323031c186247bfc023c
Priyangsu Banerjee <priyangsu26@gmail.com>
2023-04-14 12:52:49 +0530

    team

--- components/Fragments/Team.js
+++ components/Fragments/Team.js
@@ -11,8 +11,36 @@ function Team({ className }) {
         <p className="text-center text-slate-200 mt-1">
           Lorem ipsum dolor sit amet.
         </p>
-        <div className="mt-16 flex justify-center">
-          <div className="h-[290px] w-[250px] relative">
+        <div className="mt-16 flex items-center justify-center space-x-3 px-3">
+          <div className="h-[290px] w-[200px] relative">
+            <img
+              src="/teamMemberBorder.png"
+              className="absolute h-full w-full inset-0"
+              alt=""
+            />
+            <div className="absolute h-full w-full">
+              <div className="p-2">
+                <img
+                  src="/demoTeam.png"
+                  className="rounded-tr-xl rounded-tl-lg"
+                  alt=""
+                />
+              </div>
+              <div className="mt-3">
+                <h2 className="text-lg font-bold text-white text-center">
+                  Joseph
+                </h2>
+              </div>
+              <div className="flex items-center w-full mt-3 px-[1px]">
+                <div className="h-[1px] bg-gradient-to-r from-white/40 to-transparent w-full"></div>
+                <div className="shrink-0 text-white bg-white/10 px-4 py-2 rounded-lg text-sm">
+                  Devops Lead
+                </div>
+                <div className="h-[1px] bg-gradient-to-r to-white/40 from-transparent w-full"></div>
+              </div>
+            </div>
+          </div>
+          <div className="h-[290px] w-[200px] relative">
             <img
               src="/teamMemberBorder.png"
               className="absolute h-full w-full inset-0"
