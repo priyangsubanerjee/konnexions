5e67e7e3313a65f4348fc35c27814aeab4e39f0d
Yuvraj Singh <zyuvraj53@gmail.com>
2023-04-26 01:56:56 +0530

    components updated

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,6 @@
 import Image from 'next/image'
 import { Inter } from 'next/font/google'
-import NavBar from './components/navbar'
+import NavBar from '@/components/navbar'
 
 const inter = Inter({ subsets: ['latin'] })
 
