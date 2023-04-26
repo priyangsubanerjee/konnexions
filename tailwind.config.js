bdc3c469c263ca6a8859e33c0969214998d7b7fd
Raj Seth <sethraj03@gmail.com>
2023-04-26 16:40:22 +0530

    Created Team Page

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,6 +7,9 @@ module.exports = {
   ],
   theme: {
     extend: {
+      colors: {
+        card_bg:"rgba(217, 217, 217, 0.11)",
+      },
       backgroundImage: {
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
         "gradient-conic":
@@ -14,5 +17,5 @@ module.exports = {
       },
     },
   },
-  plugins: [require("tailwind-scrollbar-hide")],
+  plugins: [],
 };
