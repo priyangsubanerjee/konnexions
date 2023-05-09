438e9b6ff56867eeaa77ecede985159aee97eada
Priyangsu Banerjee <priyangsu26@gmail.com>
2023-05-09 12:14:54 +0530

    gradeint

--- pages/index.js
+++ pages/index.js
@@ -12,7 +12,7 @@ export default function Home() {
     <div className="h-screen w-screen fixed inset-0 bg-black overflow-y-auto overflow-x-hidden scrollbar-hide">
       <NavBar />
       <img
-        src="/landingGradient_1R.png"
+        src="/landingGradient_1.png"
         alt=""
         className="absolute top-0 left-0 bg-transparent h-full w-full lg:-translate-x-[10%] lg:h-auto lg:w-auto"
       />
